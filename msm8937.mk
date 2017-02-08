21ead2e9bc2a16047bac7b2e3549bc849dfbe4ec
Bharat Pawar <bpawar@codeaurora.org>
2017-02-08 19:39:53 +0530

    Build keymaster impl
    Change-Id: I84e6ac469c83de4cb8dc34ee5cac78e8b8c4a64d

--- msm8937.mk
+++ msm8937.mk
@@ -202,6 +202,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Keymaster
+PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
