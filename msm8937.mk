e6df2634f8a5383120953acedf42a5fb01dc1f0e
Bharat Pawar <bpawar@codeaurora.org>
2017-05-04 12:08:32 +0530

    camera: Add HAL1 IMPL
    Add Camera HAL1 implemetation
    
    Change-Id: I760371162966efc62467a33911d416953979e110

--- msm8937.mk
+++ msm8937.mk
@@ -119,8 +119,9 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
-    camera.device@3.2-impl
-    android.hardware.camera.provider@2.4-impl
+    camera.device@3.2-impl \
+    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
 # Display
