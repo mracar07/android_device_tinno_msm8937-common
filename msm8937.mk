fcbbbefad541518704bb05bf625358c9c872e002
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2017-07-27 15:48:33 +0530

    Update rild.libpath
    Update rild.libpath using PRODUCT_PROPERTY_OVERRIDES.
    
    Change-Id: I0af7ceea71b31c279b5579e8ff5fef31bfd05050
    CRs-Fixed:  2078970

--- msm8937.mk
+++ msm8937.mk
@@ -313,3 +313,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+
+PRODUCT_PROPERTY_OVERRIDES += rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so
