bd2c3a8c1d503a7479836a067afa5d0c8298a9d0
Brahmaji K <bkomma@codeaurora.org>
2017-07-28 23:46:43 +0530

    msm8937_64: Add keymaster and gatekeeper
    Change-Id: Ibfa1155b996d5e325886a332687c8f213816e6d0

--- msm8937.mk
+++ msm8937.mk
@@ -209,6 +209,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Keymaster and Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
 # Misc
 PRODUCT_PACKAGES += \
     libcurl
