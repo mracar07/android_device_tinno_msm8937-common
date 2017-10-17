02dfed51270bd8b279c63ec76b567d2e8399645e
Bharat Pawar <bpawar@codeaurora.org>
2017-10-17 21:30:05 +0530

    msm8937_64: Update manifest.xml
    Added entries for tui communicator and qdutils services. These
    are required for Secure UI
    
    Added services and interfaces for Thermal HAL
    
    Adding the qteeconnector HAL service
    
    Add HAL entry for wigig network performance tuner to
    manifest.xml
    
    Voiceprint: enable vendor image
    
    Adding the esepowermanager HAL service
    
    Change-Id: Ie22206a027e5d2fc4bd6861fc0a4bf35739d974c

--- msm8937.mk
+++ msm8937.mk
@@ -284,6 +284,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     sensors.msm8937
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # USB HAL
 PRODUCT_PACKAGES += android.hardware.usb@1.0-service
 
